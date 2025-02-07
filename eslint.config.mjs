48357e061c75fe4f07a1df8a28c61339d755dd9e
excel007 <eak.ekkachai@gmail.com>
2025-02-07 21:47:15 +0700

    Refactor ESLint configuration to use array format and improve plugin handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,12 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = compat.config({
-  "extends": ["next/core-web-vitals", "prettier"],
-  "plugins": ["@typescript-eslint"],
-  "rules": {
-    "@typescript-eslint/no-unused-vars": "error"
-  }
-});
-
-export default eslintConfig;
+export default [
+  ...compat.extends(["next/core-web-vitals", "prettier"]),
+  {
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+];
